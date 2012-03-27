1826fe74e14f534320aff8e46d9c276f0898871a
Paul Price <price@astro.princeton.edu>
2012-03-27 17:01:45 +0000

    Prefer tag names to branch names.

--- python/lsst/sconsUtils/vcs/hg.py
+++ python/lsst/sconsUtils/vcs/hg.py
@@ -13,11 +13,18 @@ import os, re
 #
 def guessVersionName():
     """Guess a version name"""
-
     idents = os.popen("hg id").readline()
     ident = re.split(r"\s+", idents)
     if re.search(r"\+", ident[0]):
         raise RuntimeError("Error with hg version: uncommitted changes")
-    if ident[1] == "tip":
+
+    # Prefer tag name to branch name; branch names get printed in parens
+    index = 1
+    while ident[index].startswith('(') and ident[index].endswith(')') and len(ident) > index + 1:
+        index += 1
+
+    # Prefer hash to "tip"
+    if ident[index] == "tip":
         return ident[0]
-    return ident[1]
+
+    return ident[index]
