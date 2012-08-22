b09a33c71aa18846e7c7b6bf9996c73e6601005e
Jim Bosch <jbosch@astro.princeton.edu>
2012-08-21 20:35:14 -0400

    Don't assume clang always comes from Apple.

--- python/lsst/sconsUtils/state.py
+++ python/lsst/sconsUtils/state.py
@@ -236,7 +236,7 @@ def _configureCommon():
         """Return a string identifing the compiler in use"""
         versionStrings = {"Free Software Foundation" : "gcc",
                           "Intel Corporation" : "icc",
-                          "Apple clang version" : "clang"
+                          "clang version" : "clang"
                           }
         context.Message("Checking who built the CC compiler...")
         for string, key in versionStrings.items():
