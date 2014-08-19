1044f8ab9b962cb7c780d2effb58e35fa5cef75f
Robert Lupton the Good <rhl@astro.princeton.edu>
2014-08-19 11:46:38 -0400

    Added a test that we check the error code of tests

---
+++ tests/testAll.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+# 
+# LSST Data Management System
+# Copyright 2008, 2009, 2010 LSST Corporation.
+# 
+# This product includes software developed by the
+# LSST Project (http://www.lsst.org/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the LSST License Statement and 
+# the GNU General Public License along with this program.  If not, 
+# see <http://www.lsstcorp.org/LegalNotices/>.
+#
+
+"""
+Tests for sconsUtils
+
+Run with:
+   testAll.py
+or
+   python
+   >>> import testAll; testAll.run()
+"""
+
+import eups
+import subprocess
+import unittest
+
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+class SconsUtilsTestCase(unittest.TestCase):
+    """A test case for sconsUtils"""
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testCheckErrorCode(self):
+        self.assertTrue(subprocess.call("""cd %s/tests/testFailedTests
+                                           setup -r .
+                                           scons > /dev/null 2>&1""" % eups.productDir("sconsUtils"),
+                                        shell=True), "Failed to detect failed tests")
+
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+#
+# Copied from utils/python/lsst/utils/tests.py as I don't want to make sconsUtils depend on utils
+#
+import sys
+        
+def tests_run(suite, exit=True):
+    """Exit with the status code resulting from running the provided test suite"""
+
+    if unittest.TextTestRunner().run(suite).wasSuccessful():
+        status = 0
+    else:
+        status = 1
+
+    if exit:
+        sys.exit(status)
+    else:
+        return status
+
+def suite():
+    """Returns a suite containing all the test cases in this module."""
+
+    suites = []
+    suites += unittest.makeSuite(SconsUtilsTestCase)
+    return unittest.TestSuite(suites)
+
+def run(shouldExit=False):
+    """Run the tests"""
+    tests_run(suite(), shouldExit)
+
+if __name__ == "__main__":
+    run(True)
