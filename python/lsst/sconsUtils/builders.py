lsst/sconsUtils

commit da6154c91ebe0b826ded9338f3f7d832520e8f8b

Fix lack of sorting in SourcesForSharedLibrary.
Sorting wasn't working before, leading to spurious rebuilds due to
dependency order changes when the list of top-level build targets
changes.

I'm not sure all of these changes are necessary to fix it - using
Flatten() may have been all we needed - but sorting by str certainly
makes it easier to debug (without that it's not clear how scons sorts
File objects) and always returning SharedObjects avoids having any
intermediate steps where the sort order could get messed up.

python/lsst/sconsUtils/builders.py
@@ -68,8 +68,8 @@ def SourcesForSharedLibrary(self, files):
     files = [SCons.Script.File(file) for file in files]
 
     if not (self.get("optFiles") or self.get("noOptFiles")):
-        files.sort()
-        return files
+        objs = [self.SharedObject(ccFile) for ccFile in sorted(state.env.Flatten(files), key=str)]
+        return objs
 
     if self.get("optFiles"):
         optFiles = self["optFiles"].replace(".", r"\.")  # it'll be used in an RE
@@ -96,19 +96,18 @@ def SourcesForSharedLibrary(self, files):
     CCFLAGS_OPT = re.sub(r"-O(\d|s)\s*", "-O%d " % opt, " ".join(self["CCFLAGS"]))
     CCFLAGS_NOOPT = re.sub(r"-O(\d|s)\s*", "-O0 ", " ".join(self["CCFLAGS"]))  # remove -O flags from CCFLAGS
 
-    sources = []
+    objs = []
     for ccFile in files:
         if optFilesRe and re.search(optFilesRe, ccFile.abspath):
-            self.SharedObject(ccFile, CCFLAGS=CCFLAGS_OPT)
-            ccFile = os.path.splitext(ccFile.abspath)[0] + self["SHOBJSUFFIX"]
+            obj = self.SharedObject(ccFile, CCFLAGS=CCFLAGS_OPT)
         elif noOptFilesRe and re.search(noOptFilesRe, ccFile.abspath):
-            self.SharedObject(ccFile, CCFLAGS=CCFLAGS_NOOPT)
-            ccFile = os.path.splitext(ccFile.abspath)[0] + self["SHOBJSUFFIX"]
+            obj = self.SharedObject(ccFile, CCFLAGS=CCFLAGS_NOOPT)
+        else:
+            obj = self.SharedObject(ccFile)
+        objs.append(obj)
 
-        sources.append(ccFile)
-
-    sources.sort()
-    return sources
+    objs = sorted(state.env.Flatten(sources), key=str)
+    return objs
 
 
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-

commit 032ddf45fa57139bf9869997300896655410c71a

Use correct variable
Bug introduced in da6154c91ebe0b826ded9338f3f7d832520e8f8b
when "sources" was renamed to "objs".

python/lsst/sconsUtils/builders.py
@@ -117,7 +117,7 @@ def SourcesForSharedLibrary(self, files):
             obj = self.SharedObject(ccFile)
         objs.append(obj)
 
-    objs = sorted(state.env.Flatten(sources), key=str)
+    objs = sorted(state.env.Flatten(objs), key=str)
     return objs
 
 