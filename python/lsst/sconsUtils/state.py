59b86a89a2165f46cca5ee7fe529ecec0dc8a552
Pim Schellart <P.Schellart@princeton.edu>
2017-10-12 12:23:25 -0400

    Typo

--- python/lsst/sconsUtils/state.py
+++ python/lsst/sconsUtils/state.py
@@ -422,7 +422,7 @@ def _configureCommon():
         env.Append(CCFLAGS=['-Wall'])
         env.Append(CCFLAGS=["-Wno-unknown-pragmas"])  # we don't want complaints about icc/clang pragmas
         env.Append(CCFLAGS=["-Wno-unused-local-typedefs"])  # boost generates a lot of these
-        env.Append(CCFLAGS=['-fno-lto']) # disable LTO for mismatch with conda compiled binaries
+        env.Append(CCFLAGS=['-fno-lto'])  # disable LTO for mismatch with conda compiled binaries
     elif env.whichCc == "icc":
         env.Append(CCFLAGS=['-Wall'])
         filterWarnings = {
