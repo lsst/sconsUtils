fbbefe9f4d352147900893db0a2ff4d350f5f4ee
Tim Jenness <tjenness@lsst.org>
2019-09-16 15:49:56 -0700

    Add test to check that XDG_CONFIG_CACHE is set
    This test can only be run by SCons.

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -5,6 +5,7 @@ There is nothing scons-specific about these tests.
 """
 
 import unittest
+import os
 
 
 class SimplestPossibleTestCase(unittest.TestCase):
@@ -12,6 +13,13 @@ class SimplestPossibleTestCase(unittest.TestCase):
 
     def testSimple(self):
         self.assertEqual(2 + 2, 4)
+
+    def testEnvironment(self):
+        """Test the environment. The test will fail if the tests are run
+        by anything other than SCons."""
+        envVar = "XDG_CACHE_HOME"
+        self.assertIn(envVar, os.environ)
+        self.assertTrue(os.path.exists(os.environ[envVar]), f"Check path {os.environ[envVar]}")
 
 
 if __name__ == "__main__":
