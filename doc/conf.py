4f891ffd5b134f4fbe554bf10598c870658174c6
Tim Jenness <tjenness@lsst.org>
2023-06-27 16:55:02 -0700

    Modernize sphinx config

--- doc/conf.py
+++ doc/conf.py
@@ -22,15 +22,13 @@
 
 """Sphinx configuration file for an LSST stack package.
 
-This configuration only affects single-package Sphinx documentation builds.
+This configuration only affects single-package Sphinx documenation builds.
 """
 
-from documenteer.sphinxconfig.stackconf import build_package_configs
-import lsst.sconsUtils
-import lsst.sconsUtils.version
+from documenteer.conf.pipelinespkg import *  # noqa: F403, import *
 
-
-_g = globals()
-_g.update(build_package_configs(
-    project_name="sconsUtils",
-    version=lsst.sconsUtils.version.__version__))
+project = "sconsUtils"
+html_theme_options["logotext"] = project  # noqa: F405, unknown name
+html_title = project
+html_short_title = project
+doxylink = {}
